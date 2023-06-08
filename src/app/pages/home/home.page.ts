9a870c5e4ba031aca81b0ed0bb8fc7dd3357a18d
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2023-06-08 17:10:21 +0200

    Actulizado Review

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { User, UserService } from 'src/app/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { Reviews, ReviewsFormComponent, ReviewsService, User, UserService } from 'src/app/core';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,10 @@ export class HomePage {
   esPc: boolean;
   constructor(
     public user:UserService,
-    private router:Router
+    private router:Router,
+    private reviewSvc:ReviewsService,
+    private alert: AlertController,
+    private modal: ModalController
   ) { }
 
   signOut(){
@@ -26,5 +30,29 @@ export class HomePage {
   onResize(event?) {
     this.esMovil = window.innerWidth < 768; // Si el ancho de la pantalla es mayor a 768, se considera que se está en una pantalla de escritorio
     this.esPc = window.innerWidth > 768; // Si el ancho de la pantalla es mayor a 768, se considera que se está en una pantalla de escritorio
+  }
+
+
+  async presentFormUser(reviewdata: Reviews) {
+    const modal = await this.modal.create({
+      component: ReviewsFormComponent,
+      componentProps: {
+        review: reviewdata
+      },
+    });
+  
+    modal.onDidDismiss().then(result => {
+      if (result.role === 'success') {
+        const addReview = result.data.review;
+        this.reviewSvc.addReview(addReview);
+      }
+    });
+  
+    await modal.present();
+  }
+
+
+  onAddReview(reviewdata){
+    this.presentFormUser(null);
   }
 }
