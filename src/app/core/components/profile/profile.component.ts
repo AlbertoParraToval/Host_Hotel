a319c91f760ed36219adbd739af83fa84852cb05
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2023-06-11 20:42:20 +0200

    Actualizando interfaz

--- src/app/core/components/profile/profile.component.ts
+++ src/app/core/components/profile/profile.component.ts
@@ -9,6 +9,7 @@ import { LocaleService } from '../../services/locale.service';
 import { UserService } from '../../services';
 import { Router } from '@angular/router';
 import { User } from '../../models';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * @class ProfileComponent
@@ -22,21 +23,58 @@ import { User } from '../../models';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
+  language = 1; // 0 for Spanish, 1 for English
+  currentLanguage;
   @Output() onEdit = new EventEmitter(); // Event emitted when the edit button is clicked
   @Output() onDelete = new EventEmitter(); // Event emitted when the delete button is clicked
   @Input() user: User; // The user profile to display
   esPc: boolean; // Flag indicating whether the device is a desktop
   esMovil: boolean; // Flag indicating whether the device is a mobile
+  selectedLanguage: string;
+  languageOptions: { code: string; name: string; }[];
+
 
   constructor(
     public locale: LocaleService,
     public userSvc: UserService,
-    public router: Router
+    public router: Router,
+    private translate:TranslateService
   ) { }
 
   ngOnInit() {
     this.onResize();
+    this.languageOptions = [
+      {
+        code: 'es',
+        name: 'Español',
+      },
+      {
+        code: 'en',
+        name: 'English',
+      },
+      {
+        code: 'fr',
+        name: 'Français',
+      },
+    ],
+    // Establecer el idioma seleccionado inicialmente
+    this.selectedLanguage = this.translate.getDefaultLang();
+    
+  }
+
+  onLanguageChange() {
+    // Lógica para cambiar el idioma
+    this.translate.setDefaultLang(this.selectedLanguage);
+    // Otras acciones que puedas necesitar realizar al cambiar el idioma
+  }
+
+  /**
+   * @brief Initializes the component.
+   * 
+   * Sets the default language to English.
+   */
+  private async init() {
+    this.translate.setDefaultLang('en');
   }
 
   /**
