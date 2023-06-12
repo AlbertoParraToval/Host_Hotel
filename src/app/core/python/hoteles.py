c7e759a920277070706a6e02d1133fb3d9ba9d0e
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2023-06-12 19:53:21 +0200

    Probando Configuraciones

--- src/app/core/python/hoteles.py
+++ src/app/core/python/hoteles.py
@@ -1,40 +1,35 @@
 import os
 import pandas as pd
 import json
-from collections import OrderedDict
+import csv
 
 # Obtener la ruta completa del archivo "hoteles.json"
-ruta_datos = os.path.join(os.path.dirname(__file__), '')
-ruta_hoteles_json = os.path.join(ruta_datos, 'hoteles.json')
-ruta_hoteles_csv = os.path.join(ruta_datos, 'hoteles.csv')
+ruta_hoteles = os.path.join(os.path.dirname(__file__), '')
+ruta_hoteles_json = os.path.join(ruta_hoteles, 'hoteles.json')
+ruta_hoteles_csv = os.path.join(ruta_hoteles, 'hoteles.csv')
 
-# Cargar el JSON de hoteles en una lista con OrderedDict
+# Cargar el JSON en una lista de hoteles
 with open(ruta_hoteles_json) as file:
-    data = json.load(file, object_pairs_hook=OrderedDict)
+    data = json.load(file)
 
-# Crear una lista para almacenar los datos de los hoteles
-hoteles_data = []
-
-# Iterar sobre los hoteles y extraer los datos
+# Crear una lista de diccionarios para almacenar los datos de cada atributo
+nuevos_datos = []
 for hotel in data:
-    nombre_hotel = hotel.get("name_hotel")
-    ubicacion_hotel = hotel.get("localtion_hotel").replace("Ã³", "ó")
-    url_imagen = hotel.get("url_img")
-    info_hotel = hotel.get("info_hotel")
+    nuevo_hotel = {}
+    for key, value in hotel.items():
+        if key != "id":  # Excluir la columna "id"
+            if isinstance(value, dict):
+                # Si el valor es un diccionario, convertirlo a una cadena JSON
+                nuevo_hotel[key] = json.dumps(value)
+            else:
+                # Si el valor no es un diccionario, convertirlo a cadena antes de la codificación
+                nuevo_hotel[key] = str(value)
+    nuevos_datos.append(nuevo_hotel)
 
-    hotel_data = {
-        "name_hotel": nombre_hotel,
-        "localtion_hotel": ubicacion_hotel,
-        "url_img": url_imagen,
-        "info_hotel": info_hotel
-    }
-
-    hoteles_data.append(hotel_data)
-
-# Crear DataFrame para los hoteles
-hoteles_df = pd.DataFrame(hoteles_data)
+# Crear DataFrame con los datos modificados
+hoteles_df = pd.DataFrame(nuevos_datos)
 
 # Guardar el DataFrame en el archivo CSV
-hoteles_df.to_csv(ruta_hoteles_csv, index=False, encoding='utf-8', sep=',')
+hoteles_df.to_csv(ruta_hoteles_csv, index=False, quoting=csv.QUOTE_ALL, sep=',')
 
-print("Archivo CSV creado.")
+print("Archivo CSV de hoteles creado con éxito.")
